6803774bad5523958ae220e7772a021e23730ae1
Roberto Altamirano <Roberto Altamirano@Antony>
2018-10-23 15:36:43 -0500

    Anthony

--- Operaciones/src/operaciones/Suma.java
+++ Operaciones/src/operaciones/Suma.java
@@ -9,12 +9,12 @@ package operaciones;
  *
  * @author Roberto Altamirano
  */
-public class Principal extends javax.swing.JFrame {
+public class Suma extends javax.swing.JFrame {
 
     /**
-     * Creates new form Principal
+     * Creates new form Suma
      */
-    public Principal() {
+    public Suma() {
         initComponents();
     }
 
@@ -60,20 +60,20 @@ public class Principal extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Suma.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Suma.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Suma.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Suma.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Principal().setVisible(true);
+                new Suma().setVisible(true);
             }
         });
     }
